880b987f88c0296eb5ca1da526892c9dc56aaef7
Matt Bettinson <mattbettinson@me.com>
2014-01-21 17:01:55 -0500

    Added .gitignore and renamed the package to com.github.trojanrobotics

--- src/com/github/trojanrobotics/Config.java
+++ src/com/github/trojanrobotics/Config.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.wpi.first.wpilibj.templates;
+package com.github.trojanrobotics;
 
 import edu.wpi.first.wpilibj.Servo;
 
--- src/com/github/trojanrobotics/RobotTemplate.java
+++ src/com/github/trojanrobotics/RobotTemplate.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-package edu.wpi.first.wpilibj.templates;
+package com.github.trojanrobotics;
 
 import com.sun.squawk.platform.posix.natives.Time;
 import edu.wpi.first.wpilibj.Compressor;
