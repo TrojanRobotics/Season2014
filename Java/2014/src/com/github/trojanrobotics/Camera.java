de58e3dde3f285269d70afbeac8b4ae412feeedb
Matt Bettinson <mattbettinson@Matts-MacBook-Pro.local>
2014-01-30 19:35:41 -0500

    Fixed issues from pull request

--- Java/2014/src/com/github/trojanrobotics/Camera.java
+++ Java/2014/src/com/github/trojanrobotics/Camera.java
@@ -1,25 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.trojanrobotics;
 
-import edu.wpi.first.wpilibj.AnalogChannel;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.camera.AxisCamera;
-import edu.wpi.first.wpilibj.camera.AxisCameraException;
-import edu.wpi.first.wpilibj.image.BinaryImage;
-import edu.wpi.first.wpilibj.image.ColorImage;
-import edu.wpi.first.wpilibj.image.NIVisionException;
-import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
-
-/**
- *
- * @author mattbettinson
- */
 public class Camera {
      public static class Direction
         {
@@ -44,7 +24,5 @@ public class Camera {
                 {
                         this.value = value;
                 }
-        }
-     
-     
+        }    
 }
