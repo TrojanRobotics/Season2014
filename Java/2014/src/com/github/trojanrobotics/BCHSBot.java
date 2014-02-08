77f03bd75d92ece8bcf16f134823bc1e8bc38250
Quinn Stevenson <quinnedwin96@yahoo.ca>
2014-02-08 09:57:08 -0500

    Implemented SmartDashboard
    Started smart dashboard, cleaned up various classes.

--- Java/2014/src/com/github/trojanrobotics/BCHSBot.java
+++ Java/2014/src/com/github/trojanrobotics/BCHSBot.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class BCHSBot extends IterativeRobot {
 	Joystick mainJoystick, secondaryJoystick;
@@ -13,26 +14,46 @@ public class BCHSBot extends IterativeRobot {
 	double servoAngleX;
 	double x, y;
 	Chasis chasis = new Chasis(Config.LDRIVE, Config.RDRIVE, Config.ULTRASONIC, Config.LEFT_ENCODER, Config.RIGHT_ENCODER);
-        boolean autoRunOnce;
-        
+	boolean autoRunOnce;
+	
 	public void robotInit() {
 		mainJoystick = new Joystick(Config.MAIN_JOYSTICK);
 		secondaryJoystick = new Joystick(Config.SECONDARY_JOYSTICK);
 		servoAngleY = servoY.getAngle();
 		servoAngleX = servoX.getAngle();	
+		
+		LiveWindow.addActuator("Left Side", "Value", chasis.leftSide.talOne);
+		LiveWindow.addActuator("Left Side", "Value", chasis.leftSide.talTwo);
+		LiveWindow.addActuator("Right Side", "Value", chasis.leftSide.talOne);
+		LiveWindow.addActuator("Left Side", "Value", chasis.leftSide.talTwo);
+		LiveWindow.addActuator("PID", "leftPID", chasis.leftSidePID);
+		LiveWindow.addActuator("PID", "rightPID", chasis.rightSidePID);
 	}
-        public void disabledPeriodic()
-        {
-                chasis.reset();
-                autoRunOnce = true;
-        }
-	public void autonomousPeriodic() 
-	{
-                if (autoRunOnce == true)
-                {
-                    chasis.setSetpoint(15);
-                    autoRunOnce = false;
-                }
+	
+	public void disabledPeriodic(){
+		chasis.stop();
+		//chasis.reset();
+		autoRunOnce = true;
+    }
+	
+	public void autonomousPeriodic() {
+		System.out.println("Autonomous Periodic");
+		if (autoRunOnce == true) {
+			System.out.println("Autonomous");
+			//chasis.set(0.5);
+			autoRunOnce = false;
+		}
+		String clear = "                         ";
+		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser1, 1, clear);
+		chasis.driverStationLCD.updateLCD();
+		
+		String s = Double.toString(chasis.rightSideEncoder.getDistance());
+		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser1, 1, s);
+		
+		String s1 = Double.toString(chasis.leftSideEncoder.getDistance());
+		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser2, 1, s1);
+		chasis.driverStationLCD.updateLCD();
+		
                 //once moved to firing position, release winch to fire the ball
                 //bonus: fire when hot goal
                 if (chasis.getPID() == 15)
@@ -47,24 +68,24 @@ public class BCHSBot extends IterativeRobot {
 		double joyStickY = Math.floor(mainJoystick.getY());
 		double joyStickX = Math.floor(mainJoystick.getX());
 		
-		if (joyStickY > 0.5) {
-			servoAngleY += 0.005;
-			System.out.println("Adding to the servoAngle\n");
-		} else if (joyStickY < -0.5) {
-			servoAngleY -= 0.005;
-			System.out.println("Subtracting from the servoAngle\n");
-		}
-
-		if (joyStickX > 0.5) {
-			servoAngleX += 0.005;
-			System.out.println("Adding to the servoAngle\n");
-		} else if (joyStickX < -0.5) {
-			servoAngleX -= 0.005;
-			System.out.println("Subtracting from the servoAngle\n");
-		}
-		
-		servoY.set(Lib.limitOutput(servoAngleY));
-		servoX.set(Lib.limitOutput(servoAngleX));
+//		if (joyStickY > 0.5) {
+//			servoAngleY += 0.005;
+//			System.out.println("Adding to the servoAngle\n");
+//		} else if (joyStickY < -0.5) {
+//			servoAngleY -= 0.005;
+//			System.out.println("Subtracting from the servoAngle\n");
+//		}
+//
+//		if (joyStickX > 0.5) {
+//			servoAngleX += 0.005;
+//			System.out.println("Adding to the servoAngle\n");
+//		} else if (joyStickX < -0.5) {
+//			servoAngleX -= 0.005;
+//			System.out.println("Subtracting from the servoAngle\n");
+//		}
+//		
+//		servoY.set(Lib.limitOutput(servoAngleY));
+//		servoX.set(Lib.limitOutput(servoAngleX));
 		
 		SmartDashboard.putNumber("Servo Y:", Lib.limitOutput(servoAngleY));
 		SmartDashboard.putNumber("Servo X:", Lib.limitOutput(servoAngleX));
@@ -80,13 +101,25 @@ public class BCHSBot extends IterativeRobot {
         
         chasis.rightSide.set(Lib.limitOutput(y + x));
         chasis.leftSide.set(-Lib.limitOutput(y - x));
-
+		
+//		String clear = "                         ";
+//		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser1, 1, clear);
+//		chasis.driverStationLCD.updateLCD();
+//		
+//		String s = Double.toString(chasis.rightSideEncoder.getDistance());
+//		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser1, 1, s);
+//		
+//		String s1 = Double.toString(chasis.leftSideEncoder.getDistance());
+//		chasis.driverStationLCD.println(DriverStationLCD.Line.kUser2, 1, s1);
+//		chasis.driverStationLCD.updateLCD();
+//		
+//		System.out.println("Distance and rate" + chasis.rightSideEncoder.getDistance() + "    " + chasis.rightSideEncoder.getRate());
 	}
 
 	/**
 	 * This function is called periodically during test mode
 	 */
 	public void testPeriodic() {
-		
+		SmartDashboard.putNumber("Adam", 5);
 	}
 }
--- Java/2014/src/com/github/trojanrobotics/Chasis.java
+++ Java/2014/src/com/github/trojanrobotics/Chasis.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.PIDSource;
 
 public class Chasis 
 {
@@ -13,55 +15,61 @@ public class Chasis
         Encoder leftSideEncoder, rightSideEncoder;
         Compressor compressor;
         Winch winch;
+		DriverStationLCD driverStationLCD;
         
         public Chasis(int[] rightChannels, int[] leftChannels ,int[] ultraSonicPingAndEcho, int[] leftEncoderChannels, int[] rightEncoderChannels)
         {
-                this.leftSide = new Bundle(rightChannels[0], rightChannels[1]);
-                this.rightSide = new Bundle(leftChannels[0], leftChannels[1]);
-                leftSideEncoder = new Encoder(leftEncoderChannels[0], leftEncoderChannels[1]);
-                rightSideEncoder = new Encoder(rightEncoderChannels[0], rightEncoderChannels[1]);
-                ultrasonic = new Ultrasonic(ultraSonicPingAndEcho[0], ultraSonicPingAndEcho[1]);
-                leftSidePID = new PIDController(Config.PID[0], Config.PID[1], Config.PID[2], leftSideEncoder, leftSide);
-                rightSidePID = new PIDController(Config.PID[0], Config.PID[1], Config.PID[2], rightSideEncoder, rightSide);
-                compressor = new Compressor(Config.COMPRESSOR[0], Config.COMPRESSOR[1]);
-                winch = new Winch(Config.WINCH_MOTOR, Config.WINCH_SWITCH);
-                leftSideEncoder.setDistancePerPulse(1);
-                rightSideEncoder.setDistancePerPulse(1);     
-                compressor.start();
-                leftSideEncoder.start();
-                rightSideEncoder.start();
-                
+            this.leftSide = new Bundle(leftChannels[0], leftChannels[1]);
+            this.rightSide = new Bundle(rightChannels[0], rightChannels[1]);   
+			leftSideEncoder = new Encoder(leftEncoderChannels[0], leftEncoderChannels[1]);
+            rightSideEncoder = new Encoder(rightEncoderChannels[0], rightEncoderChannels[1]);
+			leftSideEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+			rightSideEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+            leftSideEncoder.setDistancePerPulse(Config.LEFT_DRIVE_DPP);
+            rightSideEncoder.setDistancePerPulse(Config.RIGHT_DRIVE_DPP); 
+			//leftSideEncoder.setReverseDirection(true);
+			//rightSideEncoder.setReverseDirection(true);
+			//leftSideEncoder.start();
+            //rightSideEncoder.start();
+			//ultrasonic = new Ultrasonic(ultraSonicPingAndEcho[0], ultraSonicPingAndEcho[1]);
+			rightSidePID = new PIDController(Config.PID[0], Config.PID[1], Config.PID[2], rightSideEncoder, rightSide);
+			leftSidePID = new PIDController(Config.PID[0], Config.PID[1], Config.PID[2], leftSideEncoder, leftSide);
+			//compressor = new Compressor(Config.COMPRESSOR[0], Config.COMPRESSOR[1]);
+            //winch = new Winch(Config.WINCH_MOTOR, Config.WINCH_SWITCH);
+            //compressor.start();
+            //driverStationLCD = DriverStationLCD.getInstance();       
         }                                                                                                      
         
         public void set(double speed)
         {
-                leftSide.set(speed);
-                rightSide.set(-speed);
+            leftSide.set(speed);
+            rightSide.set(-speed);
         }
         
         public double getPID()
         {
-                return leftSidePID.get();
+            return leftSidePID.get();
         }
         public void reset()
         {
-                leftSideEncoder.reset();
-                rightSideEncoder.reset();
+            leftSideEncoder.reset();
+            rightSideEncoder.reset();
         }
         
         public void setSetpoint(double setPoint)
         {
+			if (!leftSidePID.isEnable()) leftSidePID.enable();
+            if (!rightSidePID.isEnable()) rightSidePID.enable();
+			
             leftSidePID.setSetpoint(setPoint);
             rightSidePID.setSetpoint(-setPoint);
-            if (!leftSidePID.isEnable()) leftSidePID.enable();
-            if (!rightSidePID.isEnable()) rightSidePID.enable();
         }
-                
+               
         public void stop()
         {
-                leftSide.stop();
-                rightSide.stop();
-                leftSidePID.disable();
-		rightSidePID.disable();
+            leftSide.stop();
+            rightSide.stop();
+            leftSidePID.disable();
+			rightSidePID.disable();
         }
 }
